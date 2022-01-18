852126abf02aaf319a3651c796fb774f0a61ff18
nika <nika.iobishvili@gmail.com>
2022-01-18 13:17:43 +0400

    second commit

--- FacebookApp/ViewController.swift
+++ FacebookApp/ViewController.swift
@@ -10,12 +10,18 @@ import UIKit
 class ViewController: UIViewController {
     
     var someText: String = "My name is Nika Iobishvili"
+    var extraText: String = "How do you do?"
 
     override func viewDidLoad() {
         super.viewDidLoad()
         sayHello()
+        askQuestion()
     }
     func sayHello() {
         print(someText)
+    }
+    func askQuestion() {
+        let question: String = someText + extraText
+        print(question)
     }
 }
